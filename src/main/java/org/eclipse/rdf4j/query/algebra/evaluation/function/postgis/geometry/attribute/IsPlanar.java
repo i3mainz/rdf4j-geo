9d392705f73768122f9727de172c91a16f3c2bee
timo.homburg <timo.homburg@hs-mainz.de>
2020-02-23 23:24:24 +0100

    Added isPlanar and isSolid

---
+++ src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/function/postgis/geometry/attribute/IsPlanar.java
@@ -0,0 +1,26 @@
+package org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.geometry.attribute;
+
+import org.eclipse.rdf4j.model.vocabulary.POSTGIS;
+import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.geometry.base.GeometricBinaryAttributeFunction;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Geometry;
+
+public class IsPlanar extends GeometricBinaryAttributeFunction {
+
+	@Override
+	public String getURI() {
+		return POSTGIS.st_isSolid.stringValue();
+	}
+
+	@Override
+	public boolean attribute(Geometry geom) {
+		Boolean is3D=true;
+        for(Coordinate coord:geom.getCoordinates()) {
+        	if(Double.isNaN(coord.getZ())) {
+        		is3D=false;
+        	}
+        }
+        return !is3D;
+	}
+
+}
