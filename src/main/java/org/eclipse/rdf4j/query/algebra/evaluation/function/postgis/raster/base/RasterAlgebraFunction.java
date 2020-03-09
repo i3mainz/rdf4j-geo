108579f5d2413705c152ed941c384d2941adea99
situx <timo.homburg@gmx.de>
2020-03-10 00:55:32 +0100

    Prepared raster algebra functions

---
+++ src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/function/postgis/raster/base/RasterAlgebraFunction.java
@@ -0,0 +1,32 @@
+package org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.raster.base;
+
+import org.apache.sis.coverage.grid.GridCoverage;
+import org.eclipse.rdf4j.model.Literal;
+import org.eclipse.rdf4j.model.Value;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.query.algebra.evaluation.ValueExprEvaluationException;
+import org.eclipse.rdf4j.query.algebra.evaluation.function.Function;
+import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.util.LiteralRegistry;
+import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.util.literals.LiteralType;
+import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.util.literals.raster.RasterLiteral;
+
+public abstract class RasterAlgebraFunction implements Function {
+
+	@Override
+	public Value evaluate(ValueFactory valueFactory, Value... args) throws ValueExprEvaluationException {
+		if (args.length != 3) {
+			throw new ValueExprEvaluationException(getURI() + " requires exactly 1 arguments, got " + args.length);
+		}
+		LiteralType l=LiteralRegistry.getLiteral(((Literal)args[0]).getDatatype().toString());
+		if(l instanceof RasterLiteral) {
+			GridCoverage geom=((RasterLiteral)l).read(args[0].stringValue());
+			GridCoverage geom2=((RasterLiteral)l).read(args[1].stringValue());
+			GridCoverage result = modify(geom,geom2);
+			return valueFactory.createLiteral(((RasterLiteral) l).unparse(result),((Literal)args[0]).getDatatype());
+		}
+		return null;
+	}
+	
+	public abstract GridCoverage modify(GridCoverage coverage, GridCoverage coverage2);
+
+}
