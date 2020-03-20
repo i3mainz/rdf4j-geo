433c1b9038ad2d20139815a05325051f07026ce2
situx <timo.homburg@gmx.de>
2020-03-20 01:57:15 +0100

    Densify fix

--- src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/function/postgis/geometry/transform/Densify.java
+++ src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/function/postgis/geometry/transform/Densify.java
@@ -1,5 +1,6 @@
 package org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.geometry.transform;
 
+import org.eclipse.rdf4j.model.vocabulary.POSTGIS;
 import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.geometry.base.GeometricModifierDoubleFunction;
 import org.locationtech.jts.densify.Densifier;
 import org.locationtech.jts.geom.Geometry;
@@ -8,14 +9,13 @@ public class Densify extends GeometricModifierDoubleFunction {
 
 	@Override
 	public String getURI() {
-		// TODO Auto-generated method stub
-		return null;
+		return POSTGIS.ST_DENSIFY.stringValue();
 	}
 
 	@Override
 	protected Geometry relation(Geometry geom, Double tolerance) {
-	        Geometry result=Densifier.densify(geom, tolerance);
-	        return result;
+	   Geometry result=Densifier.densify(geom, tolerance);
+	   return result;
 	}
 
 }
