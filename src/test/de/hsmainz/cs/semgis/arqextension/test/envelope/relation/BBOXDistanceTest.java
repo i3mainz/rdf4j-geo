a6c501c4059913d4f008489e0ed657b103745f9e
timo.homburg <timo.homburg@hs-mainz.de>
2020-04-02 20:14:03 +0200

    BBOX tests

---
+++ src/test/de/hsmainz/cs/semgis/arqextension/test/envelope/relation/BBOXDistanceTest.java
@@ -0,0 +1,33 @@
+package de.hsmainz.cs.semgis.arqextension.test.envelope.relation;
+
+import static org.junit.Assert.assertEquals;
+
+import org.eclipse.rdf4j.model.Value;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.envelope.relation.BBOXDistance;
+import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.util.literals.vector.WKTDatatype;
+import org.junit.jupiter.api.Test;
+
+public class BBOXDistanceTest {
+
+	public static final String testGeom1="LINESTRING (1 4, 1 7)";
+	
+	public static final String testGeom2="LINESTRING (0 0, 4 2)";
+	
+	public static final String testGeom3="LINESTRING (6 1, 6 5)";
+	
+	public static final String testGeom4="LINESTRING (2 3, 5 6)";
+	
+	@Test
+	public void testBBOXDistance() {
+		BBOXDistance instance=new BBOXDistance();
+		ValueFactory valfac=SimpleValueFactory.getInstance();
+		Value geo=valfac.createLiteral(testGeom1, WKTDatatype.LiteralIRI);
+		Value geo2=valfac.createLiteral(testGeom2, WKTDatatype.LiteralIRI);
+		Value result=instance.evaluate(valfac, geo,geo2);
+		Value expResult=valfac.createLiteral(2);
+		assertEquals(expResult, result);
+	}
+	
+}
---
+++ src/test/de/hsmainz/cs/semgis/arqextension/test/point/constructor/MakePointTest.java
@@ -0,0 +1,38 @@
+package de.hsmainz.cs.semgis.arqextension.test.point.constructor;
+
+import static org.junit.Assert.assertEquals;
+
+import org.eclipse.rdf4j.model.Value;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.point.constructor.MakePoint;
+import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.util.literals.vector.WKTDatatype;
+import org.junit.jupiter.api.Test;
+
+public class MakePointTest {
+
+	public static final String testPoint1="POINT(-71.1043443253471, 42.3150676015829)";
+	
+	public static final String testPointM="POINT M(1, 2,1.5)";
+	
+	@Test
+	public void testMakePoint1() {
+		MakePoint instance=new MakePoint();
+		ValueFactory valfac=SimpleValueFactory.getInstance();
+		Value geo=valfac.createLiteral(testPoint1, WKTDatatype.LiteralIRI);
+		Value result=instance.evaluate(valfac, geo);
+		Value expResult=valfac.createLiteral(testPoint1, WKTDatatype.LiteralIRI);
+		assertEquals(expResult, result);
+	}
+	
+	@Test
+	public void testMakePointXYZ() {
+		MakePoint instance=new MakePoint();
+		ValueFactory valfac=SimpleValueFactory.getInstance();
+		Value geo=valfac.createLiteral(testPointM, WKTDatatype.LiteralIRI);
+		Value result=instance.evaluate(valfac, geo);
+		Value expResult=valfac.createLiteral(testPointM, WKTDatatype.LiteralIRI);
+		assertEquals(expResult, result);
+	}
+	
+}
