f1421796a22ec190f58a6637235ae02ca9f45ce0
situx <timo.homburg@gmx.de>
2020-04-02 02:04:29 +0200

    Relation tests

---
+++ src/test/de/hsmainz/cs/semgis/arqextension/test/geometry/relation/EqualTypeTest.java
@@ -0,0 +1,43 @@
+package de.hsmainz.cs.semgis.arqextension.test.geometry.relation;
+
+import static org.junit.Assert.assertEquals;
+
+import org.eclipse.rdf4j.model.Value;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.geometry.attribute.NumDistinctPoints;
+import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.geometry.relation.EqualType;
+import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.util.literals.vector.WKTDatatype;
+import org.junit.jupiter.api.Test;
+
+
+public class EqualTypeTest {
+	
+	public static final String testGeom="LINESTRING(743238 2967416,743238 2967450,743265 2967450, 743265.625 2967416,743238 2967416)";
+
+	public static final String testGeom2="MULTIPOINT (0 1, 1 0, 2 1)";
+
+	
+	@Test
+	public void testEqual() {
+		EqualType instance=new EqualType();
+		ValueFactory valfac=SimpleValueFactory.getInstance();
+		Value geo=valfac.createLiteral(testGeom, WKTDatatype.LiteralIRI);
+		Value geo2=valfac.createLiteral(testGeom, WKTDatatype.LiteralIRI);
+		Value result=instance.evaluate(valfac, geo,geo2);
+		Value expResult=valfac.createLiteral(true);
+	    assertEquals(expResult, result);
+	}
+	
+	@Test
+	public void testNotEqual() {
+		EqualType instance=new EqualType();
+		ValueFactory valfac=SimpleValueFactory.getInstance();
+		Value geo=valfac.createLiteral(testGeom, WKTDatatype.LiteralIRI);
+		Value geo2=valfac.createLiteral(testGeom2, WKTDatatype.LiteralIRI);
+		Value result=instance.evaluate(valfac, geo,geo2);
+		Value expResult=valfac.createLiteral(false);
+	    assertEquals(expResult, result);
+	}
+
+}
