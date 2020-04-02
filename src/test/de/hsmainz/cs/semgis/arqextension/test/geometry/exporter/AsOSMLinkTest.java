14fbc4ed847b12f042547f10c5db6206f2089513
situx <timo.homburg@gmx.de>
2020-04-02 22:58:51 +0200

    Merge

--- src/test/de/hsmainz/cs/semgis/arqextension/test/geometry/exporter/AsOSMLinkTest.java
+++ src/test/de/hsmainz/cs/semgis/arqextension/test/geometry/exporter/AsOSMLinkTest.java
@@ -1,4 +1,4 @@
-package de.hsmainz.cs.semgis.arqextension.test.geometry.attribute;
+package de.hsmainz.cs.semgis.arqextension.test.geometry.exporter;
 
 import static org.junit.Assert.assertEquals;
 
@@ -6,17 +6,17 @@ import org.eclipse.rdf4j.model.Value;
 import org.eclipse.rdf4j.model.ValueFactory;
 import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import org.eclipse.rdf4j.model.impl.ValueFactoryImpl;
-import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.geometry.attribute.Is3D;
 import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.geometry.attribute.IsSolid;
+import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.geometry.exporter.AsOSMLink;
 import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.util.literals.vector.WKTDatatype;
 import org.junit.Test;
 
-public class IsSolidTest {
+public class AsOSMLinkTest {
 
 
 @Test
-public void testIsSolidFalse() {
-	IsSolid is3d=new IsSolid();
+public void testAsOSMLink() {
+	AsOSMLink is3d=new AsOSMLink();
 	ValueFactory valfac=SimpleValueFactory.getInstance();
 	Value geo=valfac.createLiteral("<http://www.opengis.net/def/crs/EPSG/0/27700> MULTIPOINT (10 40, 40 30, 20 20, 30 10)", WKTDatatype.LiteralIRI);
 	Value result=is3d.evaluate(valfac, geo);
