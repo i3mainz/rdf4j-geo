608214010ecd1c65bffd152c389550caf0219cda
timo.homburg <timo.homburg@hs-mainz.de>
2020-04-02 19:42:57 +0200

    AsTWKB

---
+++ src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/function/postgis/geometry/exporter/AsTWKB.java
@@ -0,0 +1,21 @@
+package org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.geometry.exporter;
+
+import org.eclipse.rdf4j.model.vocabulary.POSTGIS;
+import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.geometry.base.GeometricStringExportFunction;
+import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.util.literals.vector.TWKBDatatype;
+import org.locationtech.jts.geom.Geometry;
+
+public class AsTWKB extends GeometricStringExportFunction {
+
+	@Override
+	public String getURI() {
+		return POSTGIS.ST_ASTWKB.stringValue();
+	}
+
+	@Override
+	public String operation(Geometry geom) {
+		TWKBDatatype lit=new TWKBDatatype();
+		return lit.unparse(geom);
+	}
+
+}
