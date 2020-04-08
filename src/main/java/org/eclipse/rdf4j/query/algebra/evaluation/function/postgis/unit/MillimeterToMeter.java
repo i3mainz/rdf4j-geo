bce8ed648ca3e46fa7eb0a2560aee987c767f75e
timo.homburg <timo.homburg@hs-mainz.de>
2020-04-08 20:52:20 +0200

    Moved unit functions to new package

---
+++ src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/function/postgis/unit/MillimeterToMeter.java
@@ -0,0 +1,26 @@
+package org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.unit;
+
+import org.eclipse.rdf4j.model.Value;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.vocabulary.POSTGIS;
+import org.eclipse.rdf4j.query.algebra.evaluation.ValueExprEvaluationException;
+import org.eclipse.rdf4j.query.algebra.evaluation.function.Function;
+
+public class MillimeterToMeter implements Function {
+
+	@Override
+	public String getURI() {
+		// TODO Auto-generated method stub
+		return POSTGIS.KilometerToMeter.stringValue();
+	}
+
+	@Override
+	public Value evaluate(ValueFactory valueFactory, Value... args) throws ValueExprEvaluationException {
+		if (args.length != 1) {
+			throw new ValueExprEvaluationException(getURI() + " requires exactly 1 arguments, got " + args.length);
+		}			
+		Double value=Double.valueOf(args[0].stringValue());
+		return valueFactory.createLiteral(value/1000);
+	}
+
+}
