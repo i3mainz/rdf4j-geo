1e210495c4c2462201cb1d67890fc333f0356e84
timo.homburg <timo.homburg@hs-mainz.de>
2020-04-11 23:02:31 +0200

    More tests and fixes

---
+++ src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/function/postgis/raster/attribute/PixelHeight.java
@@ -0,0 +1,21 @@
+package org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.raster.attribute;
+
+import org.apache.sis.coverage.grid.GridCoverage;
+import org.eclipse.rdf4j.model.vocabulary.POSTGIS;
+import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.raster.base.RasterAttributeIntIntIntBooleanFunction;
+
+public class PixelHeight extends RasterAttributeIntIntIntBooleanFunction {
+
+	@Override
+	public String getURI() {
+		return POSTGIS.st_pixelHeight.stringValue();
+	}
+
+
+	@Override
+	public Boolean attribute(GridCoverage coverage, Integer noband, Integer x, Integer y) {
+		int[] valuess=new int[coverage.getSampleDimensions().size()];
+		return coverage.render(null).getTile(x, y).getHeight();
+	}
+
+}
---
+++ src/test/de/hsmainz/cs/semgis/arqextension/test/geometry/constructor/GeomFromTextTest.java
@@ -0,0 +1,28 @@
+package de.hsmainz.cs.semgis.arqextension.test.geometry.constructor;
+
+import static org.junit.Assert.assertEquals;
+
+import org.eclipse.rdf4j.model.Value;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.geometry.constructor.GeomFromText;
+import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.util.literals.vector.WKTDatatype;
+import org.junit.jupiter.api.Test;
+import org.locationtech.jts.io.ParseException;
+
+
+public class GeomFromTextTest {
+
+	public static final String geoJsonTestGeom="POINT (-48.23456,20.12345)";
+	
+	@Test
+	public void testGeomFromGeoJSON() throws ParseException {
+        GeomFromText instance=new GeomFromText();
+        ValueFactory valfac=SimpleValueFactory.getInstance();
+		Value geo=valfac.createLiteral(geoJsonTestGeom, WKTDatatype.LiteralIRI);
+		Value result=instance.evaluate(valfac,geo);
+		Value expResult=valfac.createLiteral(geoJsonTestGeom, WKTDatatype.LiteralIRI);
+		assertEquals(expResult, result);
+	}
+	
+}
