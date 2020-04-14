0abf53f8386c0f861b40766410c461f5c5fdd278
timo.homburg <timo.homburg@hs-mainz.de>
2020-04-14 16:56:27 +0200

    Added BandMin and BandMax

--- src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/function/postgis/raster/attribute/BandMax.java
+++ src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/function/postgis/raster/attribute/BandMax.java
@@ -1,5 +1,20 @@
 package org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.raster.attribute;
 
-public class BandMax {
+import org.apache.sis.coverage.grid.GridCoverage;
+import org.eclipse.rdf4j.model.vocabulary.POSTGIS;
+import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.raster.base.RasterAttributeDoubleIntFunction;
+
+public class BandMax extends RasterAttributeDoubleIntFunction {
+
+	@Override
+	public String getURI() {
+		// TODO Auto-generated method stub
+		return POSTGIS.ST_BANDMAX.stringValue();
+	}
+
+	@Override
+	public Double attribute(GridCoverage coverage, Integer bandno) {
+		return coverage.getSampleDimensions().get(bandno).getSampleRange().get().getMaxDouble();
+	}
 
 }
--- src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/function/postgis/raster/attribute/BandMin.java
+++ src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/function/postgis/raster/attribute/BandMin.java
@@ -1,5 +1,20 @@
 package org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.raster.attribute;
 
-public class BandMin {
+import org.apache.sis.coverage.grid.GridCoverage;
+import org.eclipse.rdf4j.model.vocabulary.POSTGIS;
+import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.raster.base.RasterAttributeDoubleIntFunction;
+
+public class BandMin extends RasterAttributeDoubleIntFunction {
+
+	@Override
+	public String getURI() {
+		// TODO Auto-generated method stub
+		return POSTGIS.ST_BANDMin.stringValue();
+	}
+
+	@Override
+	public Double attribute(GridCoverage coverage, Integer bandno) {
+		return coverage.getSampleDimensions().get(bandno).getSampleRange().get().getMinDouble();
+	}
 
 }
