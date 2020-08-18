b60982fdca14866db1289ea391b2f9eecb14ddc0
timo.homburg <timo.homburg@hs-mainz.de>
2020-08-18 19:46:59 +0200

    More bbox tests

---
+++ src/test/org/eclipse/rdf4j/testsuites/postgis/envelope/relation/BBOXIntersectsTest.java
@@ -0,0 +1,44 @@
+package org.eclipse.rdf4j.testsuites.postgis.envelope.relation;
+
+import static org.junit.Assert.assertEquals;
+
+import org.eclipse.rdf4j.model.Value;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.envelope.relation.BBOXIntersects;
+import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.util.literals.vector.WKTDatatype;
+import org.junit.jupiter.api.Test;
+
+public class BBOXIntersectsTest {
+
+public static final String testGeom1="LINESTRING (1 4, 1 7)";
+	
+	public static final String testGeom2="LINESTRING (0 0, 4 2)";
+	
+	public static final String testGeom3="LINESTRING (6 1, 6 5)";
+	
+	public static final String testGeom4="LINESTRING (2 3, 5 6)";
+	
+	@Test
+	public void testBBOXIntersectsTrue() {
+		BBOXIntersects instance=new BBOXIntersects();
+		ValueFactory valfac=SimpleValueFactory.getInstance();
+		Value geo=valfac.createLiteral(testGeom1, WKTDatatype.LiteralIRI);
+		Value geo2=valfac.createLiteral(testGeom2, WKTDatatype.LiteralIRI);
+		Value result=instance.evaluate(valfac, geo2,geo);
+		Value expResult=valfac.createLiteral(2);
+		assertEquals(expResult, result);
+	}
+	
+	@Test
+	public void testBBOXIntersectsFalse() {
+		BBOXIntersects instance=new BBOXIntersects();
+		ValueFactory valfac=SimpleValueFactory.getInstance();
+		Value geo=valfac.createLiteral(testGeom1, WKTDatatype.LiteralIRI);
+		Value geo2=valfac.createLiteral(testGeom2, WKTDatatype.LiteralIRI);
+		Value result=instance.evaluate(valfac, geo,geo2);
+		Value expResult=valfac.createLiteral(2);
+		assertEquals(expResult, result);
+	}
+	
+}
