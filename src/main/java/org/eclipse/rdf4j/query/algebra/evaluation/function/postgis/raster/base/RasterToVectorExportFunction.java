5a58ee4c5fa168f124adca6ede926f3edfd58777
situx <timo.homburg@gmx.de>
2020-10-13 14:03:48 +0200

    Base function fix

--- src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/function/postgis/raster/base/RasterToVectorExportFunction.java
+++ src/main/java/org/eclipse/rdf4j/query/algebra/evaluation/function/postgis/raster/base/RasterToVectorExportFunction.java
@@ -9,6 +9,7 @@ import org.eclipse.rdf4j.query.algebra.evaluation.function.Function;
 import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.util.LiteralRegistry;
 import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.util.literals.LiteralType;
 import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.util.literals.raster.RasterLiteral;
+import org.eclipse.rdf4j.query.algebra.evaluation.function.postgis.util.literals.vector.VectorLiteral;
 import org.locationtech.jts.geom.Geometry;
 
 public abstract class RasterToVectorExportFunction implements Function {
@@ -24,7 +25,7 @@ public abstract class RasterToVectorExportFunction implements Function {
 			Integer longitude = Integer.valueOf(args[1].stringValue());
 			Integer latitude = Integer.valueOf(args[2].stringValue());
 			Geometry result = modify(geom);
-			return valueFactory.createLiteral(((RasterLiteral) l).unparse(result),((Literal)args[0]).getDatatype());
+			return valueFactory.createLiteral(((VectorLiteral) l).unparse(result),((Literal)args[0]).getDatatype());
 		}
 		return null;
 	}
